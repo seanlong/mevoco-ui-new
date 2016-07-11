a21d56c30213f5cfa5be72e2484aa047060ef07b
root <root@localhost.localdomain>
2016-07-11 08:43:07 -0400

    websocket

---
+++ grails-app/controllers/zstackui/HelloController.groovy
@@ -0,0 +1,17 @@
+package zstackui
+
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+
+class HelloController {
+
+    def index() {
+        render(view: "index")
+    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/hello")
+    protected String hello(String world) {
+        return "hello from controller, ${world}!"
+    }
+}
